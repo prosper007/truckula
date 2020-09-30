ad074bae6d147c73f541b874efd4bbbf2668c1c3
prosper007 <onungwap@gmail.com>
2020-09-30 00:19:22 -0400

    form fields in state

--- src/containers/RequestTruck/RequestTruck.js
+++ src/containers/RequestTruck/RequestTruck.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import PageHeader from '../../components/UI/PageHeader/PageHeader';
-// import Input from '../../components/UI/Input/Input';
+import Input from '../../components/UI/Input/Input';
 import styles from './RequestTruck.module.css';
 
 class RequestTruck extends Component {
+  textInput = {
+    elementType: 'input',
+    elementConfig: {
+      type: 'text',
+    },
+    value: '',
+    validation: {
+      required: true
+    },
+    valid: false,
+    touched: false,
+  }
+
+  addressForm = {
+    addressLine1: { ...this.textInput },
+    addressLine2: { ...this.textInput },
+    city: { ...this.textInput },
+    state: { ...this.textInput },
+    zipCode: { ...this.textInput },
+  }
+
   state = {
     selectedTruck: {
       name: 'Small Straight',
@@ -20,21 +41,42 @@ class RequestTruck extends Component {
       description: "This truck is old. It's as old as Kevin."
     },
     requestForm: {
-
+      name: { ...this.textInput },
+      email: { ...this.textInput },
+      pickupLocation: { ...this.addressForm },
+      dropoffLocation: { ...this.addressForm },
     }
   }
 
   render() {
+    let formElements = [];
+    for (let key in this.state.requestForm) {
+      formElements.push({
+        id: key,
+        ...this.state.requestForm[key],
+      })
+    }
     let form = (
       <form>
-
+        {formElements.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.elementType}
+            elementConfig={formElement.elementConfig}
+            value={formElement.value}
+            invalid={!formElement.valid}
+            shouldValidate={formElement.validation}
+            touched={formElement.touched}
+          // changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
       </form>
     );
     return (
       <React.Fragment>
         <PageHeader>Request Truck {this.state.selectedTruck.letter}</PageHeader>
         <div className={styles.RequestForm}>
-
+          {form}
         </div>
       </React.Fragment>
     )
